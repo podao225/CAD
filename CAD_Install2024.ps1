568f5d8024c9eb51b0eccb98ab6c25a7932c5dcd
podao225 <103050316+podao225@users.noreply.github.com>
2026-03-16 23:49:37 +0800

    Update CAD_Install2024.ps1

--- CAD_Install2024.ps1
+++ CAD_Install2024.ps1
@@ -87,7 +87,7 @@ if (-not $skipAll) {
 
         $startTime = Get-Date
         $lastSize = 0
-        $totalSizeEst = 3.58 * 1GB
+        $totalSizeEst = 2.94 * 1GB
 
         while ($job.State -eq "Running") {
             if (Test-Path $ZipPath -PathType Leaf) {
